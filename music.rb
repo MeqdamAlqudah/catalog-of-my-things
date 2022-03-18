a7f338f4cb777578f30b14943ccd29957af7f1ee
MeqdamAlqudah <memuq56@gmail.com>
2022-03-18 16:31:24 +0200

    Add json file

--- music.rb
+++ music.rb
@@ -11,4 +11,19 @@ class MusicAlbum < Items
   def can_music_be_archived?()
     (can_be_archived? && @on_spotity)
   end
+  def to_string
+    "Publish date #{@publish_date} On spotity: #{@on_spotity} Archived: #{@archived}"
+  end
+
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'a' => [on_spotity, publish_date, archived]
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(*object['a'])
+  end
 end
